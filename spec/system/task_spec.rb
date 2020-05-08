94da5304c6e689f26cbbe48419b26a8f80394f44
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-08 12:49:35 +0000

    Add tests for new task function and create task function

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   describe 'タスク一覧画面' do
     context 'タスクを作成した場合' do
-      it '作成済みのタスクが表示される'
+      it '作成済みのタスクが表示される' do
+        task = FactoryBot.create(:task, name: 'task')
+        visit tasks_path
+        expect(page).to have_content 'task'
+      end
     end
   end
   describe 'タスク登録画面' do
     context '必要項目を入力して、createボタンを押した場合' do
-      it 'データが保存される'
+      it 'データが保存される' do
+        visit new_task_path
+        fill_in 'Name', with: 'task_name'
+        fill_in 'Content', with: 'task_content'
+        click_on '登録'
+        expect(page).to have_content 'task_' && 'name' && 'content'
+      end
     end
   end
   describe 'タスク詳細画面' do
