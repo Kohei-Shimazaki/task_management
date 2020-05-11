136ca7afe54342561802b2de18a44e16eb223bc3
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-11 09:01:59 +0000

    Add index to Users email

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+                    name: "Bob",
+                    email: "bob@gmail.com",
+                    password_digest: "password",
+)
