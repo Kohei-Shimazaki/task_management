5347260ad3ca477063b4f287fcdb331b79b2de4f
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-12 09:17:43 +0000

    Add admin users controllers and views

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,42 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.all.includes(:tasks)
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "ユーザ作成しました！"
+      session[:user_id] = @user.id
+      redirect_to admin_user_path(@user.id)
+    else
+      render :new
+    end
+  end
+  def show
+    @user = User.find(params[:id])
+  end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "ユーザ情報を更新しました！"
+      redirect_to tasks_path
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:notice] = "ユーザを削除しました！"
+    redirect_to admin_users_path
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   root to: "tasks#index"
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
+  namespace :admin do
+    resources :users
+  end
 end
