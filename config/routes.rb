792a28633bd321d89e4cb89565bfc9d655a4490c
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-12 04:33:10 +0000

    Add log-in view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :tasks
   root to: "tasks#index"
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
