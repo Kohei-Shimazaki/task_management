1597fec2656b293b925a176f4c25f11a64b916a6
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-12 05:45:20 +0000

    Don't jump to tasks and user profiles until login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  before_action :authenticate_user
+  private
+  def authenticate_user
+    unless logged_in?
+      flash[:notice] = "ログインはこの画面からお願いします。"
+      redirect_to new_session_path
+    end
+  end
 end
