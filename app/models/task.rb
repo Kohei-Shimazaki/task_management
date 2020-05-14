858d745fc3661a0b4e0afe7d64c9dcd039f82034
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-14 06:45:43 +0000

    Add association between Task and Label

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,15 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: {maximum: 30}
   validates :content, presence: true
+  enum priority: [:低, :中, :高]
+
+  belongs_to :user
+  has_many :labelings, dependent: :destroy
+  has_many :labels, through: :lagbellings, source: :label
+
   scope :search_like_name, ->(word) {where("name LIKE ?", "%#{ word }%")}
   scope :search_status, ->(status) {where(status: status)}
   scope :order_deadline, -> {order(deadline: :desc)}
   scope :order_priority, -> {order(priority: :desc)}
   scope :order_created_at, -> {order(created_at: :desc)}
-  enum priority: [:低, :中, :高]
-  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_061753) do
+ActiveRecord::Schema.define(version: 2020_05_14_063244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labelings", force: :cascade do |t|
+    t.integer "task_id"
+    t.integer "label_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "title", null: false
