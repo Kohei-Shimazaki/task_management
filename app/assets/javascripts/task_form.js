67505388f2093744bef177fc3e80a67bc48c6495
kohei shimazaki <kohei.shimazaki.k@gmail.com>
2020-05-23 02:10:35 +0000

    Add comments

--- app/assets/javascripts/task_form.js
+++ app/assets/javascripts/task_form.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /*ステータスが完了のタスクをグレーに変更*/
   $(".status").each(function() {
     if($(this).text() == "完了") {
       $(this).parent().css("background-color", "lightgray");
